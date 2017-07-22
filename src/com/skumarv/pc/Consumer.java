a52c9bca82f2378724f7442306315da4b7d53b6c
Saravanakumar <skumarv@hotmail.com>
2017-07-23 00:49:40 +0530

    Just formatting

--- src/com/skumarv/pc/Consumer.java
+++ src/com/skumarv/pc/Consumer.java
@@ -7,10 +7,10 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
-public class Consumer <T extends ProducerWorker<U>, U> extends Thread {
+public class Consumer<T extends ProducerWorker<U>, U> extends Thread {
 	private CubbyHole<U> cubbyhole;
 	private List<Producer<U>> producerThreadLst = null;
-	private SubmitTasks<?, U> prodConsTest = null;
+	private SubmitTasks<T, U> prodConsTest = null;
 
 	public List<Producer<U>> getProducerThreadLst() {
 		return producerThreadLst;
@@ -20,7 +20,8 @@ public class Consumer <T extends ProducerWorker<U>, U> extends Thread {
 		this.producerThreadLst = producerThreadLst;
 	}
 
-	public Consumer(CubbyHole<U> c, int number, List<Producer<U>> producerThreadLst, SubmitTasks<?, U> prodConsTest) {
+	public Consumer(CubbyHole<U> c, int number,
+			List<Producer<U>> producerThreadLst, SubmitTasks<T, U> prodConsTest) {
 		cubbyhole = c;
 		this.producerThreadLst = producerThreadLst;
 		this.prodConsTest = prodConsTest;
@@ -28,41 +29,41 @@ public class Consumer <T extends ProducerWorker<U>, U> extends Thread {
 
 	public void run() {
 		ProducerResponse<U> value = null;
-		Iterable<Iterable<Producer<U>>> splitList = chunk(producerThreadLst, 2);
-		for (Iterable<Producer<U>> iterable : splitList) {
-			for (Producer<U> producer : iterable) {
-				producer.start();
-			}
-			Set<Producer<U>> set = new HashSet<Producer<U>>();
-			boolean gotResponse = false;
-			U response = null;
-			int siz = 0;
-			if (iterable instanceof Collection<?>) {
-				siz = ((Collection<?>) iterable).size();
-			}
-			for (int i = 0; i < siz; i++) {
-				value = cubbyhole.get();
-				if (value.getProducer() != null) {
-					set.add(value.getProducer());
-				}
-				if (value.getValue() != null) {
-					gotResponse = true;
-					response = value.getValue();
-					break;
-				}
-				// System.out.println("Consumer #" + this.number + " got: " +
-				// value);
-			}
-			if (gotResponse) {
+		if (producerThreadLst != null && !producerThreadLst.isEmpty()) {
+			Iterable<Iterable<Producer<U>>> splitList = chunk(
+					producerThreadLst, 2);
+			for (Iterable<Producer<U>> iterable : splitList) {
 				for (Producer<U> producer : iterable) {
-					if (!set.contains(producer)) {
-						producer.interrupt();
+					producer.start();
+				}
+				Set<Producer<U>> set = new HashSet<Producer<U>>();
+				boolean gotResponse = false;
+				U response = null;
+				int siz = 0;
+				if (iterable instanceof Collection<?>) {
+					siz = ((Collection<?>) iterable).size();
+				}
+				for (int i = 0; i < siz; i++) {
+					value = cubbyhole.get();
+					if (value.getProducer() != null) {
+						set.add(value.getProducer());
+					}
+					if (value.getValue() != null) {
+						gotResponse = true;
+						response = value.getValue();
+						break;
 					}
 				}
-				prodConsTest.notifyAll(response);
-				break;
-			}
-			//System.out.println(iterable);
+				if (gotResponse) {
+					for (Producer<U> producer : iterable) {
+						if (!set.contains(producer)) {
+							producer.interrupt();
+						}
+					}
+					prodConsTest.notifyAll(response);
+					break;
+				}
+			}// all chunks, sublist processing end
 		}
 	}
 
